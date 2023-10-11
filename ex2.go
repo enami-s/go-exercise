c29db8f1db720c4cdecda02a279394aa535e5419
江波俊介 <enamishunsuke@XC230B0140.local>
2023-10-11 17:56:23 +0900

    rename

---
+++ ex2.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+const (
+	zerotenmax = 11
+	zerofifmax = 51
+)
+
+var wg sync.WaitGroup
+
+func zeroten() {
+	defer wg.Done()
+	for i := 0; i < zerotenmax; i++ {
+		fmt.Println("zeroten: ", i)
+	}
+}
+
+func zerofifty() {
+	defer wg.Done()
+	for i := 0; i < zerofifmax; i++ {
+		fmt.Println("zerofif: ", i)
+	}
+}
+
+func main() {
+	wg.Add(2)
+	go zeroten()
+	go zerofifty()
+	fmt.Println("hoge")
+
+	wg.Wait()
+	fmt.Println("finish")
+}
